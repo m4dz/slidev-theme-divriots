8c7996454afc3cd4c7f32e50babb58ea2a9a045d
m4dz <code@m4dz.net>
2021-11-04 18:42:35 +0100

    Quote layout: add support for cite and author props

--- layouts/quote.vue
+++ layouts/quote.vue
@@ -1,19 +1,39 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import Default from './default.vue'
 
 const props = defineProps<{
-    class?: string;
+  class?: string;
+  cite?: string;
+  author?: string;
 }>()
+
+const style = computed(() => {
+  return `background-image:url(${props.author})`;
+});
 </script>
 
 <template>
   <Default class="quote" :class="class">
+    <span class="author" :style="style" v-if="author" />
     <slot />
+    <cite v-if="cite">{{ cite }}</cite>
   </Default>
 </template>
 
 <style scoped>
 .quote {
   @apply text-3xl leading-12;
+}
+
+cite {
+  @apply mt-8;
+  @apply text-xl;
+}
+
+.author {
+  @apply absolute top-10 right-14;
+  @apply w-50 h-50 bg-center bg-cover;
+  @apply rounded-full ring-inset ring-6 ring-light-900 ring-opacity-50;
 }
 </style>
